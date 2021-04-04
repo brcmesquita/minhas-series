b17f85970391c62cd6a6072bddb3779e02ad4642
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2021-04-04 09:31:22 -0300

    fim dia 01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Generos from "./Generos";
+import Home from "./Home";
+import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NovoGenero from "./NovoGenero";
+import EditarGenero from "./EditarGenero"
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("/api").then((res) => {
+      setData(res.data);
+    });
+  }, []);
   return (
-    <div className="">
-      <h1>Minhas Séries</h1>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/generos/:id" exact component={EditarGenero} />
+        <Route path="/generos/novo" exact component={NovoGenero} />
+        <Route path="/generos" exact component={Generos} />
+      </div>
+    </Router>
   );
 }
 
