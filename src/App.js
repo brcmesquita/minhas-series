08bfd9e4bb0d2435d8fd164ca2408ae2f1b502ec
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2021-04-04 22:26:34 -0300

    add some frontend

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Header from "./Header";
 import Generos from "./Generos";
 import Home from "./Home";
 import axios from "axios";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NovoGenero from "./NovoGenero";
-import EditarGenero from "./EditarGenero"
+import EditarGenero from "./EditarGenero";
+import Series from "./Series";
+import NovaSerie from "./NovaSerie"
+import InfoSerie from "./InfoSerie"
 
 function App() {
   const [data, setData] = useState({});
@@ -18,10 +21,18 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/generos/:id" exact component={EditarGenero} />
-        <Route path="/generos/novo" exact component={NovoGenero} />
-        <Route path="/generos" exact component={Generos} />
+        <Switch>
+          {" "}
+          // para escolher uma única rota
+          <Route path="/" exact component={Home} />
+          <Route path="/generos" exact component={Generos} />
+          <Route path="/generos/novo" exact component={NovoGenero} />
+          <Route path="/generos/:id" exact component={EditarGenero} />
+          <Route path="/series" exact component={Series} />
+          <Route path="/series/novo" exact component={NovaSerie} />
+          <Route path="/series/:id" exact component={InfoSerie} />
+
+        </Switch>
       </div>
     </Router>
   );
